125a27f1958d649e30b6c180b63d5198ee01f58f
tubaknr <97946563+tubaknr@users.noreply.github.com>
2023-07-16 09:22:39 +0300

    Update client.py

--- scripts/client.py
+++ scripts/client.py
@@ -6,13 +6,13 @@ from service.srv import addTwoInts
 
 #type of service = addTwoInts
 
-def client(a,b):
+def client(a, b):
     rospy.wait_for_service("add_two_ints")
 
     try:
         #handle for calling the service
-        add_two_ints = rospy.ServiceProxy("add_two_ints", addTwoInts) #handle = add_two_ints 
-        response = add_two_ints(a,b) #call handle like a normal fcn.
+        add_two_ints = rospy.ServiceProxy("add_two_ints", addTwoInts) # handle = add_two_ints 
+        response = add_two_ints(a, b) # call handle like a normal fcn.
         return response 
     except rospy.ServiceException as e:
         print(f"Service call failed : {e}")
@@ -25,14 +25,12 @@ def usage():
 if __name__ == "__main__":
     rospy.init_node("client")
     
-    if len(sys.argv) == 3: #3 argüman olduğunda
+    if len(sys.argv) == 3: 
         a = int(sys.argv[1])
         b = int(sys.argv[2])
     else:
         print(usage())
         sys.exit(1)
 
-    print(f"Requesting {a}+{b}")
-    print(f"{a}+{b}={client(a,b)}")
-
-
+    print(f"Requesting {a} + {b}")
+    print(f"{a} + {b} = {client(a, b)}")
